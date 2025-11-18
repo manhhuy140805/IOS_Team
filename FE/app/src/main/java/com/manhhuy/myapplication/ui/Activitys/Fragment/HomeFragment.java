65f1e60cd9dd03a24c6fd7e2167437648cb3f568
hieuvolaptrinh <vndhieuak@gmail.com>
2025-11-18 17:07:16 +0700

    Refactor HomeFragment layout and implement RecyclerViews for categories and events

--- FE/app/src/main/java/com/manhhuy/myapplication/ui/Activitys/Fragment/HomeFragment.java
+++ FE/app/src/main/java/com/manhhuy/myapplication/ui/Activitys/Fragment/HomeFragment.java
@@ -1,4 +1,4 @@
-package com.manhhuy.myapplication;
+package com.manhhuy.myapplication.ui.Activitys.Fragment;
 
 import android.os.Bundle;
 
@@ -8,57 +8,76 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.manhhuy.myapplication.R;
+
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private ActivityHomeBinding binding;
+    private CategoriesAdapter categoriesAdapter;
+    private EventAdapter eventAdapter;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        EdgeToEdge.enable(this);
+
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        setupCategoriesRecyclerView();
+        setupEventsRecyclerView();
+        loadData();
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    private void setupCategoriesRecyclerView() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        binding.categoriesRecyclerView.setLayoutManager(layoutManager);
+
+        categoriesAdapter = new CategoriesAdapter(new ArrayList<>());
+        binding.categoriesRecyclerView.setAdapter(categoriesAdapter);
     }
+
+    private void setupEventsRecyclerView() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        binding.eventsRecyclerView.setLayoutManager(layoutManager);
+
+        eventAdapter = new EventAdapter(new ArrayList<>());
+        binding.eventsRecyclerView.setAdapter(eventAdapter);
+    }
+
+    private void loadData() {
+        // Load categories data
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category("Environment", 128, R.drawable.ic_launcher_background));
+        categories.add(new Category("Education", 93, R.drawable.ic_launcher_background));
+        categories.add(new Category("Healthcare", 67, R.drawable.ic_launcher_background));
+        categories.add(new Category("Animal Care", 45, R.drawable.ic_launcher_background));
+
+        categoriesAdapter.setCategories(categories);
+
+        // Load events data
+        List<Event> events = new ArrayList<>();
+        events.add(new Event(
+                "Beach Cleanup",
+                "Green Vietnam",
+                "Environment",
+                R.drawable.ic_launcher_background,
+                "200,000 VND/day",
+                "Vung Tau"));
+        events.add(new Event(
+                "Teach English to Kids",
+                "Education For All",
+                "Teaching",
+                R.drawable.ic_launcher_background,
+                "150,000 VND/shift",
+                "Ho Chi Minh City"));
+
+        eventAdapter.setEvents(events);
+    }
+
 }
