02a3d9ef6c9170b8af649948007ccd2071626ecd
hieuvolaptrinh <vndhieuak@gmail.com>
2025-11-18 16:22:20 +0700

    change main to login

--- FE/app/src/main/java/com/manhhuy/myapplication/ui/Activitys/MainActivity.java
+++ FE/app/src/main/java/com/manhhuy/myapplication/ui/Activitys/MainActivity.java
@@ -1,43 +1,32 @@
 package com.manhhuy.myapplication.ui.Activitys;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.appbar.MaterialToolbar;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.manhhuy.myapplication.R;
+import com.manhhuy.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        // Initialize views
-        MaterialToolbar toolbar = findViewById(R.id.toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-
-        // Setup Toolbar
-        setSupportActionBar(toolbar);
-        toolbar.setOnMenuItemClickListener(item -> {
-            int id = item.getItemId();
-            if (id == R.id.action_search) {
-                Toast.makeText(this, "Search clicked", Toast.LENGTH_SHORT).show();
-                return true;
-            } else if (id == R.id.action_settings) {
-                Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show();
-                return true;
-            }
-            return false;
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
 
-        // Setup FAB
-        fab.setOnClickListener(view -> Snackbar.make(view, "FAB Clicked - Material Design 3!", Snackbar.LENGTH_LONG)
-                .setAction("Action", v -> Toast.makeText(this, "Action clicked", Toast.LENGTH_SHORT).show())
-                .show());
+        binding.btnSignIn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+        });
     }
 }
