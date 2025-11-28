ca9d450cac418180cf20f9b35d30d8e8975d9ce7
hieuvolaptrinh <vndhieuak@gmail.com>
2025-11-28 22:07:13 +0700

    refactor admin, user file

---
+++ FE/app/src/main/java/com/manhhuy/myapplication/adapter/AdminAdapter.java
@@ -0,0 +1,46 @@
+package com.manhhuy.myapplication.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.manhhuy.myapplication.ui.Activities.Fragment.Admin.AdminApprovePostsFragment;
+import com.manhhuy.myapplication.ui.Activities.Fragment.Admin.AdminFragment;
+import com.manhhuy.myapplication.ui.Activities.Fragment.Common.EventManageFragment;
+import com.manhhuy.myapplication.ui.Activities.Fragment.Admin.AdminRewardFragment;
+
+import com.manhhuy.myapplication.ui.Activities.Fragment.Common.MeFragment;
+
+public class AdminAdapter extends FragmentStateAdapter {
+
+    public AdminAdapter(FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new AdminFragment();
+            case 1:
+                return new EventManageFragment();
+            case 2:
+                return new AdminRewardFragment();
+            case 3:
+                return new AdminApprovePostsFragment();
+            case 4:
+                return new MeFragment();
+            default:
+                return new AdminFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        // Admin has 5 tabs, Customer has 4 tabs
+        return 5;
+    }
+}
